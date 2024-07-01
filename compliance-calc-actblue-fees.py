d46e48a854fca2526e1a74b7a189303bf8022d0b
Ashwin Ramaswami <aramaswamis@gmail.com>
2024-07-01 10:00:15 -0400

    Fix actblue compliance script

--- compliance-calc-actblue-fees.py
+++ compliance-calc-actblue-fees.py
@@ -1,15 +1,15 @@
 import csv
-import json
-from bs4 import BeautifulSoup
-import re
-from diskcache import Cache
-from tqdm import tqdm
-from openpyxl import load_workbook
-from datetime import datetime, timedelta
+
+"""
+INSTRUCTIONS:
+- Go to "Downloads" and download "Merchant Account Activity" CSV. Filter it manually to fit the right date range.
+- Then update the file name and run this script.
+"""
 
 # python get-emails.py
 
-actblue_file = 'ashwin-ramaswami-153140-account_activity_2024-02-01_2024-05-01.csv'
+# actblue_file = 'ashwin-ramaswami-153140-account_activity_2024-02-01_2024-05-01.csv'
+actblue_file = 'ashwin-ramaswami-153140-account_activity_2024-05-01_2024-06-30.csv'
 
 stripe_fees_total = 0
 actblue_fees_total = 0
@@ -17,15 +17,14 @@ actblue_fees_total = 0
 with open(actblue_file, 'r') as f:
     csv_reader = csv.DictReader(f)
     for row in csv_reader:
-        if row['Transaction Type'] == 'charge':
+        if row['Transaction Type'] in ('charge', 'refund', 'dispute'):
             stripe_fees_total += float(row['Stripe Fee Amount'].strip('$'))
             actblue_fees_total += float(row['Actblue Fee Amount'].strip('$'))
         elif 'actblue_fee_credit' in row['Transaction Type']:
+            # Actblue refunds the Actblue fee for refunds.
             actblue_fees_total -= float(row['Actblue Fee Amount'].strip('$'))
-        elif row['Transaction Type'] == 'refund':
-            pass
         else:
             raise Exception(row)
 
-print('stripe_fees_total', stripe_fees_total)
-print('actblue_fees_total', actblue_fees_total)
+print('stripe_fees_total', '${:.2f}'.format(stripe_fees_total))
+print('actblue_fees_total', '${:.2f}'.format(actblue_fees_total))
